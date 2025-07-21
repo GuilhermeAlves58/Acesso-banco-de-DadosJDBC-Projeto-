de39bf5c57822866fd19753ec78be97e6df0d127
perkles <guilhermealves5194@gmail.com>
2025-07-21 19:00:39 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,12 @@
 package application;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
-import db.DB;
-import db.DbIntegrityException;
-import model.entites.Department;
+import java.util.Date;
+import model.entites.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-
+		Seller seller = new Seller(1,"sandy", "sandy@gmail.com",new Date(),200.0);
+		System.out.println(seller);
 	}
 }
