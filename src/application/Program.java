5550ddaec5301016fe1cab950c81360d121d4e44
perkles <guilhermealves5194@gmail.com>
2025-07-21 18:48:24 -0300

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,16 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import db.DB;
+import db.DbIntegrityException;
+import model.entites.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+	}
+}
