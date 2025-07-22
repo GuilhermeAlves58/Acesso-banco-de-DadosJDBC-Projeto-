424810fc5873a8fbd37586b947fb719d36621a96
perkles <guilhermealves5194@gmail.com>
2025-07-22 18:33:14 -0300

    FindById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,16 @@
 package application;
 
-import java.util.Date;
+
+
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entites.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Seller seller = new Seller(1,"sandy", "sandy@gmail.com",new Date(),200.0);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 	}
 }
